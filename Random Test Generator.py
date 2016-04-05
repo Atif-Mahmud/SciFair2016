d80d4d214662e788bdad0b3d736c7177323ead5c
zmct <zmct@rogers.com>
2016-04-04 23:23:53 -0400

    Added random test generating script
    Just in case.

---
+++ Random Test Generator.py
@@ -0,0 +1,46 @@
+import random
+def makenums():
+	for i in range(0, 30):
+		num = random.randint(1, 21)
+		if num == 1:
+			print("CH-")
+		elif num == 2:
+			print("CH")
+		elif num==3:
+			print("CH+")
+		elif num==4:
+			print("|<<")
+		elif num==5:
+			print(">>|")
+		elif num==6:
+			print(">||")
+		elif num==7:
+			print("-")
+		elif num==8:
+			print("plus")
+		elif num==9:
+			print("EQ")
+		elif num==10:
+			print("0")
+		elif num==11:
+			print("100+")
+		elif num==12:
+			print("200+")
+		elif num==13:
+			print("1")
+		elif num==14:
+			print("2")
+		elif num==15:
+			print("3")
+		elif num==16:
+			print("4")
+		elif num==17:
+			print("5")
+		elif num==18:
+			print("6")
+		elif num==19:
+			print("7")
+		elif num==20:
+			print("8")
+		elif num==21:
+			print("9")
